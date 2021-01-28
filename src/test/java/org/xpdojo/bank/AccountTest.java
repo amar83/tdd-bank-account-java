b87f2d18d16dbe921e68f2d93981ad8498c705f6
amar.pandey@gmail.com <amar.pandey@gmail.com>
2021-01-28 15:12:02 +0000

    pass - 1

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void instantiateAccont(){
+        Account account = new Account();
+        assertThat(account.balance == 0);
     }
+
+    @Test
+    public void depositAnAmountToIncreaseTheBalance() {
+        /*assertThat("your first test").isBlank();*/
+        Account account = new Account();
+        account.balance += 100;
+        assertThat(account.balance == 100);
+    }
+
+
 }
